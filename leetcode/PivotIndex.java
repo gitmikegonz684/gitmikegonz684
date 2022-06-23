1883cb8076b21b0aaa6ddf2970671d0533b36b9a
Mike Gonzalez <michaeg2@cisco.com>
2022-06-22 22:19:45 -0700

    improved solution for pivot index

--- leetcode/PivotIndex.java
+++ leetcode/PivotIndex.java
@@ -75,4 +75,14 @@ public class PivotIndex {
         } 
         return output;
     }
+
+    public int pivotIndexLite(int[] nums) {
+        int sum = 0, leftsum = 0;
+        for (int num: nums) sum += num;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
+        }
+        return -1;
+    }
 }
