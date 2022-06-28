4507522ae6873cab3a5ee15be1a6bc56bccf80c7
Mike Gonzalez <michaeg2@cisco.com>
2022-06-28 09:50:55 -0700

    map to string using stream

--- leetcode/Utils.java
+++ leetcode/Utils.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Utils {
     public static void main(String[] args) {
         System.out.println("HELLO FROM UTILS");
@@ -15,5 +18,10 @@ public class Utils {
         }
         sb.append("]");
         return sb.toString();
+    }
+
+    public static String mapToString(Map<Character,Character> map) {
+        String string = map.keySet().stream().map(key -> key + "=" + map.get(key)).collect(Collectors.joining(", ", "{", "}"));
+        return string;
     }
 }
